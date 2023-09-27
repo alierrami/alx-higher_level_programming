db03c85ba4f508b0b2f891c0248a427bae2de50a
ALI <alierrami2024@gmail.com>
2023-09-27 08:43:14 -0700

    Exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def safe_print_division(a, b):
-    try:
-        rslt = a / b
-    except:
-        rslt = None
-    finally:
-        print("Inside result: {}".format(rslt))
-    return rslt
