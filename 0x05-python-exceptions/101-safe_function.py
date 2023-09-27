ce773acc95a3e3a1f137754de249dbbb3924dce1
ALI <alierrami2024@gmail.com>
2023-09-27 08:56:47 -0700

    Exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
 import sys
-
-
 def safe_function(fct, *args):
     """Executes a function safely.
 
@@ -20,3 +17,4 @@ def safe_function(fct, *args):
     except:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
+
